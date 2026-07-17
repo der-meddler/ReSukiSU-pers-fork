db2c67c41471088c60e726d9e12b7726650528ec
thatpainterfromaustria <njilasalexander36@gmail.com>
2026-07-18 01:06:22 +0200

    Change KernelSU repository to a different fork
    Signed-off-by: thatpainterfromaustria <njilasalexander36@gmail.com>

--- kernel/setup.sh
+++ kernel/setup.sh
@@ -42,7 +42,7 @@ setup_kernelsu() {
 	echo "[+] Setting up KernelSU..."
 	# Clone the repository and rename it to KernelSU
 	if [ ! -d "$GKI_ROOT/KernelSU" ]; then
-		git clone https://github.com/ReSukiSU/ReSukiSU KernelSU
+		git clone https://github.com/der-meddler/ReSukiSU-pers-fork KernelSU
 		echo "[+] Repository cloned."
 	fi
 	cd "$GKI_ROOT/KernelSU"
@@ -84,7 +84,7 @@ setup_submodule() {
 	fi
 
     echo '[+] Setting up KernelSU as submodule...'
-    git submodule add https://github.com/ReSukiSU/ReSukiSU KernelSU || echo '[!] Failed to add KernelSU as a submodule.'
+    git submodule add https://github.com/der-meddler/ReSukiSU-pers-fork KernelSU || echo '[!] Failed to add KernelSU as a submodule.'
     echo '[+] Done.'
 }
 
